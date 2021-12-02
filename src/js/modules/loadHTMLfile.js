756b839b231a7e6cd01ebf194d3c5ce9ef970b45
Anthony Pineau <anthonypineau85@gmail.com>
2021-12-03 00:37:01 +0100

    new template

--- src/js/modules/loadHTMLfile.js
+++
@@ -1,9 +0,0 @@
-export function loadHTMLFile(name){
-    fetch("../../src/views/"+name+".html")
-    .then(response => {
-      return response.text()
-    })
-    .then(data => {
-      document.querySelector("#app").innerHTML = data;
-    });
-}
--- src/js/modules/variables.js
+++
@@ -1,5 +0,0 @@
-const nav = document.querySelector("nav");
-const openmenu = document.querySelector(".open-menu");
-const links = document.querySelectorAll(".link")
-
-export { nav, openmenu, links };
